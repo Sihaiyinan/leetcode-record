bdb0e83c467164e80c8cb78edd2672e01c7f3747
Sihaiyinan <xu.wh@outlook.com>
2020-02-24 12:50:02 +0800

    鹰击长空，鱼翔浅底，万类霜天竞自由。

---
+++ 剑指offer系列/面试题26. 树的子结构/源.cpp
@@ -0,0 +1,50 @@
+// 面试题26. 树的子结构
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {};
+};
+
+class Solution {
+public:
+	/*
+	思路：
+	第一步，首先在A中查找与根结点相同的结点
+	第二步，如果结点相同，判断其是否为子树
+	**/
+	bool isSubStructure(TreeNode* A, TreeNode* B) {
+		if (A == nullptr || B == nullptr)
+			return false;
+
+		if (A->val == B->val)
+			return sub_tree(A, B);
+		else
+			return isSubStructure(A->left, B) || isSubStructure(A->right, B);
+	}
+
+	bool sub_tree(TreeNode *A, TreeNode *B)
+	{
+		if (B == nullptr)
+			return true;
+		if (A == nullptr)
+			return false;
+		if (A->val != B->val)
+			return false;
+		return sub_tree(A->left, B->left) && sub_tree(A->right, B->right);
+	}
+};
+
+int main() 
+{
+
+	std::cout << endl;
+	return 0;
+}
