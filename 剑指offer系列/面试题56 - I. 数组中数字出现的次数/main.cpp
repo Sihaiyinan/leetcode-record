4d921c1be8b463633bb693813f562d4baf1324fe
Sihaiyinan <xu.wh@outlook.com>
2020-03-08 16:15:49 +0800

    update

---
+++ 剑指offer系列/面试题56 - I. 数组中数字出现的次数/main.cpp
@@ -0,0 +1,55 @@
+﻿// 面试题56 - I. 数组中数字出现的次数
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+
+class Solution {
+public:
+	vector<int> singleNumbers1(vector<int>& nums) {
+		vector<int> re;
+		map<int, int> m;
+		for (auto &num : nums)
+		{
+			++m[num];
+		}
+		for (auto it = m.begin(); it != m.end(); ++it)
+		{
+			if (it->second == 1)
+				re.push_back(it->first);
+		}
+		return re;
+	}
+
+	// 位运算
+	vector<int> singleNumbers(vector<int>& nums) {
+		int xor = 0;
+		vector<int> ans = { 0, 0 };   // 需要返回两位数，正好相当于0和1
+
+		for (auto &num : nums)
+			xor ^= num;        // 此时xor相当于是两个不同的数之间异或
+
+		int low = xor ^ (xor -1) & xor;  // 找出两个数的二进制中不同的最低位
+		for (auto &num : nums)
+			ans[!(num & low)] ^= num;
+		return ans;
+	}
+
+};
+
+
+
+
+int main()
+{
+	vector<int> a = { 4,7,4,1 }, re;
+	re = Solution().singleNumbers(a);
+
+
+	std::cout << std::endl;
+	return 0;
+}
---
+++ 剑指offer系列/面试题56 - II. 数组中数字出现的次数 II/main.cpp
@@ -0,0 +1,57 @@
+﻿// 面试题56 - II. 数组中数字出现的次数 II
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+
+class Solution {
+public:
+	int singleNumber1(vector<int>& nums) {
+		map<int, int> m;
+		for (auto &num : nums)
+		{
+			++m[num];
+		}
+		for (auto it = m.begin(); it != m.end(); ++it)
+		{
+			if (it->second == 1)
+				return it->first;
+		}
+		return 0;
+	}
+
+	int singleNumber(vector<int>& nums) {
+		int bit[32] = { 0 };
+		for (int i = 0; i < nums.size(); ++i)
+		{
+			unsigned int bitmask = 1;
+			for (int j = 31; j >= 0; --j)
+			{
+				int b = nums[i] & bitmask;
+				if (b != 0)
+					bit[j] += 1;
+				bitmask = bitmask << 1;
+			}
+		}
+		int res = 0;
+		for (int i = 0; i < 32; ++i)
+		{
+			res = res << 1;
+			res += bit[i] % 3;
+		}
+		return res;
+	}
+};
+
+
+
+int main()
+{
+
+	std::cout << std::endl;
+	return 0;
+}
