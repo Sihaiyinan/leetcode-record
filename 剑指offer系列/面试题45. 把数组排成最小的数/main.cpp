479efd457edd6ae8e186967b5e7414c064b6cd57
Sihaiyinan <xu.wh@outlook.com>
2020-03-10 23:14:45 +0800

    把数组排成最小的数

---
+++ 剑指offer系列/面试题45. 把数组排成最小的数/main.cpp
@@ -0,0 +1,54 @@
+﻿// 面试题45. 把数组排成最小的数
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+	static bool compare(string a, string b)
+	{
+		string t = a;
+		while (t.size() % b.size() != 0)
+		{
+			t += a;
+		}
+		string s = b;
+		while (s.size() < t.size())
+		{
+			s += b;
+		}
+		return t < s;
+	}
+
+	string minNumber(vector<int>& nums) {
+		if (nums.empty())
+			return "";
+		vector<string> s;
+		for (auto &i : nums)
+		{
+			s.push_back(to_string(i));
+		}
+		sort(s.begin(), s.end(), compare);
+
+		string re;
+		for (auto i : s)
+			re += i;
+
+		return re;
+	}
+};
+
+
+int main()
+{
+	vector<int> a = { 3,30,34,9,5 };
+	string s = Solution().minNumber(a);
+
+	std::cout << std::endl;
+	return 0;
+}
