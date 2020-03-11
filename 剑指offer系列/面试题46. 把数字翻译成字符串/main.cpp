3db9578ce723458ffe860fb9bf6b9edfeac461b4
Sihaiyinan <xu.wh@outlook.com>
2020-03-11 22:34:17 +0800

    把数字翻译成字符串

---
+++ 剑指offer系列/面试题46. 把数字翻译成字符串/main.cpp
@@ -0,0 +1,32 @@
+﻿// 面试题46. 把数字翻译成字符串
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+	// 回溯
+	int translateNum(int num) {
+		if (num < 10)
+			return 1;
+		int tmp = num % 100;
+		if (tmp < 10 || tmp > 25)
+			return translateNum(num / 10);
+		else
+			return translateNum(num / 10) + translateNum(num / 100);
+
+	}
+};
+
+
+int main()
+{
+	vector<int> a = { 3,30,34,9,5 };
+
+	std::cout << std::endl;
+	return 0;
+}
