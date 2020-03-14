ac5dac2772739ffa529397ed6a156e87cfadd0df
Sihaiyinan <xu.wh@outlook.com>
2020-03-14 23:19:35 +0800

    update

---
+++ 剑指offer系列/面试题61. 扑克牌中的顺子/main.cpp
@@ -0,0 +1,31 @@
+﻿// 面试题61. 扑克牌中的顺子
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isStraight(vector<int>& nums) {
+		int zero = 0;
+		sort(nums.begin(), nums.end());
+		for (int i = 0; i < nums.size() - 1; ++i)
+		{
+			if (nums[i] == 0) { zero++; continue; }
+			if (nums[i] == nums[i + 1]) return false;
+			if (nums[i] != nums[i + 1] + 1)zero -= nums[i + 1] - nums[i] - 1;
+		}
+		return zero >= 0;
+	}
+};
+
+
+int main()
+{
+
+	std::cout << std::endl;
+	return 0;
+}
