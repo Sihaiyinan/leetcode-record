65106fa54e70297d8d1832b8619a6186ea421718
Sihaiyinan <xu.wh@outlook.com>
2020-03-28 12:00:06 +0800

    59. 螺旋矩阵 II

---
+++ C++/59. 螺旋矩阵 II/main.cpp
@@ -0,0 +1,48 @@
+﻿// 59. 螺旋矩阵 II
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> generateMatrix(int n) {
+		vector<vector<int>> res, move;
+		move = { { 0,1 },{ 1,0 },{ 0,-1 },{ -1,0 } };
+		for (int i = 0; i < n; ++i)
+		{
+			vector<int> t(n);
+			res.push_back(t);
+		}
+
+		int i = 0, j = -1, c = 1, mi = 0;
+		while (c <= n*n)
+		{
+			auto m = move[mi % 4];
+			i = i + m[0];
+			j = j + m[1];
+			if (i >= 0 && i < n && j >= 0 && j < n && res[i][j] == 0)
+			{
+				res[i][j] = c;
+				++c;
+			}
+			else
+			{
+				i -= m[0];
+				j -= m[1];
+				++mi;
+			}
+		}
+		return res;
+	}
+};
+
+int main()
+{
+
+
+	return 0;
+}
