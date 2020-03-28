8d59f6949c6c5942ac0110eafaeea3b231d13b1c
Sihaiyinan <xu.wh@outlook.com>
2020-03-28 17:16:12 +0800

    89. 格雷编码

---
+++ C++/89. 格雷编码/main.cpp
@@ -0,0 +1,33 @@
+﻿// 89. 格雷编码
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+	vector<int> grayCode(int n) {
+		vector<int> res = { 0 };
+		int t = 1;
+
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = res.size() - 1; j >= 0; --j)
+			{
+				res.push_back(t + res[j]);
+			}
+			t <<= 1;
+		}
+		return res;
+	}
+};
+
+int main()
+{
+
+	return 0;
+}
