93bdefad59f2cccb3c69930fed12daa614f3b553
Sihaiyinan <xu.wh@outlook.com>
2020-03-28 12:07:55 +0800

    62. 不同路径

---
+++ C++/62. 不同路径/main.cpp
@@ -0,0 +1,35 @@
+﻿// 62. 不同路径
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int uniquePaths(int m, int n) {
+		if (m < 1 || n < 1) return -1;
+		vector<int> dp(n, 1);
+		for (int i = 1; i < m; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+			{
+				if (j == 0) dp[j] = 1;
+				else
+				{
+					dp[j] += dp[j - 1];
+				}
+			}
+		}
+		return dp[n - 1];
+	}
+};
+
+int main()
+{
+
+
+	return 0;
+}
