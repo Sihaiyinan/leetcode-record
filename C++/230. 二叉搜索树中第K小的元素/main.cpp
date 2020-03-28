01c2a7ab3a152d6e3215dd1e3d919db96fab1bd0
Sihaiyinan <xu.wh@outlook.com>
2020-03-28 12:29:49 +0800

    230. 二叉搜索树中第K小的元素

---
+++ C++/230. 二叉搜索树中第K小的元素/main.cpp
@@ -0,0 +1,47 @@
+﻿// 230. 二叉搜索树中第K小的元素
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	int flag = 0;
+	int kthSmallest(TreeNode* root, int k) {
+		int val = 0;
+		int cur = 0;
+		bianli(root, k, cur, val);
+		return val;
+	}
+
+	void bianli(TreeNode *root, int k, int &cur, int &val)
+	{
+		if (root == nullptr) return;
+		bianli(root->left, k, cur, val);
+		if (flag == 1) return;
+		++cur;
+		if (cur == k) {
+			val = root->val;
+			flag = 1;
+			return;
+		}
+		bianli(root->right, k, cur, val);
+	}
+};
+
+int main()
+{
+
+
+	return 0;
+}
