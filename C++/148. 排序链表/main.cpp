8c43f538ec9ccd1fe47e6f6b5ea35b8ca01d5fa3
Sihaiyinan <xu.wh@outlook.com>
2020-03-28 15:36:04 +0800

    148. 排序链表

---
+++ C++/148. 排序链表/main.cpp
@@ -0,0 +1,50 @@
+﻿// 148. 排序链表
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	static int compare(const void *a, const void *b)
+	{
+		return *(int *)a - *(int *)b;
+	}
+
+	ListNode* sortList(ListNode* head) {
+		if (head == nullptr) return head;
+		vector<int> l;
+
+		ListNode *tmp = head;
+		while (tmp)
+		{
+			l.push_back(tmp->val);
+			tmp = tmp->next;
+		}
+
+		qsort(l.data(), l.size(), sizeof(int), compare);
+		tmp = head;
+		for (auto i : l)
+		{
+			tmp->val = i;
+			tmp = tmp->next;
+		}
+		return head;
+	}
+};
+
+int main()
+{
+
+	return 0;
+}
