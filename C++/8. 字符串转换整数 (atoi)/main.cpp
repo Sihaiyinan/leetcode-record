76922c027ca984394700cf99dbc967357508b2f6
Sihaiyinan <xu.wh@outlook.com>
2020-03-30 23:51:12 +0800

    8. 字符串转换整数 (atoi)

--- C++/8. 字符串转换整数 (atoi)/main.cpp
+++ C++/8. 字符串转换整数 (atoi)/main.cpp
@@ -7,27 +7,45 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string str) {
-        // 数字的格式可以用 A[.[B]][e|EC]表示
-        // 其中A和C都是整数（可以有符号）， B是一个无符号整数
-
         if (str.empty()) return 0;
         int pos = 0;
         while (pos < str.size() && str[pos] == ' ') ++pos; // 去除前段空格
 
-
-
+        return get_integer(str, pos);
     }
 
-    int get_integer(const string &str, int &p)
+    long long get_unsigned_integer(const string &str, int &p, int flag)
     {
-        if (str[p] == '+' || str[p] == '-')
+        long long res = 0;
+        int overflow = 0;
+        while (p < str.size() && str[p] >= '0' && str[p] <= '9')
+        {
+            res = res * 10 + (str[p] - '0');
+            if (res > 0x7FFFFFFF || res < (int)0x80000000)
+            {
+                overflow = 1;
+                break;
+            }
             ++p;
-        return get_unsigned_integer(str, p);
+        }
+        if (overflow)
+        {
+            if (flag == 1) return 0x7FFFFFFF;
+            else if (flag == -1) return (int)0x80000000;
+        }
+        return res * flag;
     }
-    int get_unsigned_integer(const string &str, int &p)
+    long long get_integer(const string &str, int &p)
     {
-        int before = p;
-
+        int flag = 1;
+        if (str[p] == '+')
+            ++p;
+        else if (str[p] == '-')
+        {
+            flag = -1;
+            ++p;
+        }
+        return get_unsigned_integer(str, p, flag);
     }
 };
 
