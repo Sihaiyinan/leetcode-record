53fbc8c0ac80af1a3b6061a2b8801834646a7f14
Sihaiyinan <xu.wh@outlook.com>
2020-04-10 15:15:46 +0800

    24. 两两交换链表中的节点

---
+++ C++/24. 两两交换链表中的节点/源.cpp
@@ -0,0 +1,61 @@
+﻿// 24. 两两交换链表中的节点
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+struct ListNode
+{
+	int val; 
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+	ListNode* swapPairs(ListNode* head) {
+		if (head == nullptr) return head;
+		if (head->next == nullptr) return head;
+
+		ListNode *root = new ListNode(0);
+		root->next = head;
+		ListNode *p = root;
+
+		ListNode *a = head, *b = head->next;
+
+
+		while (b->next && b->next->next)
+		{
+			a->next = b->next;
+			b->next = a;
+			p->next = b;
+
+			ListNode *t = a;
+			a = b;
+			b = t;
+
+			p = b;
+			a = a->next->next;
+			b = b->next->next;
+		}
+
+		a->next = b->next;
+		b->next = a;
+		p->next = b;
+
+		p = root->next;
+		delete root;
+		return p;
+	}
+};
+
+
+int main()
+{
+
+	return 0;
+}
