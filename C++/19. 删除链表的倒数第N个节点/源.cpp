f18d92439469947aeb7d19ad7bfc7cc43874f4e1
Sihaiyinan <xu.wh@outlook.com>
2020-04-10 11:26:18 +0800

    19. 删除链表的倒数第N个节点

---
+++ C++/19. 删除链表的倒数第N个节点/源.cpp
@@ -0,0 +1,45 @@
+﻿// 19. 删除链表的倒数第N个节点
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+struct ListNode
+{
+	int val; 
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+		ListNode *first, *second;
+		first = head; second = head;
+		for (int i = 0; i < n; ++i)
+		{
+			second = second->next;
+		}
+		if (second == NULL)
+			return head->next;
+		while (second->next != NULL)
+		{
+			first = first->next;
+			second = second->next;
+		}
+		first->next = first->next->next;
+		return head;
+	}
+};
+
+
+int main()
+{
+
+
+	return 0;
+}
