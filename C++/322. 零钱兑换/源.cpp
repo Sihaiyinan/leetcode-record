c115143b7afb94e8f63f57c509367c77302f39bb
Sihaiyinan <xu.wh@outlook.com>
2020-04-23 22:42:45 +0800

    C++，数据结构，数字图像处理，平差原理，数字摄影测量，计算机图形学，面向对

---
+++ C++/322. 零钱兑换/源.cpp
@@ -0,0 +1,36 @@
+﻿// 322. 零钱兑换
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+	int coinChange(vector<int>& coins, int amount) {
+		int Max = amount + 1;
+		vector<int> dp(amount + 1, Max);
+		dp[0] = 0;
+
+		for (int i = 1; i <= amount; ++i)
+		{
+			for (auto c : coins)
+			{
+				if (c <= i)
+					dp[i] = min(dp[i], dp[i - c] + 1);
+			}
+		}
+		return dp[amount] > amount ? -1 : dp[amount];
+	}
+};
+
+
+int main()
+{
+	int n = 10;
+
+	return 0;
+}
