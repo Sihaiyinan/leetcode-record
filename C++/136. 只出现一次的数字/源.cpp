b7accfa77aab901ad70efd5a9514ff54b14a0309
Sihaiyinan <xu.wh@outlook.com>
2020-05-14 10:20:18 +0800

    136. 只出现一次的数字

---
+++ C++/136. 只出现一次的数字/源.cpp
@@ -0,0 +1,30 @@
+﻿// 136. 只出现一次的数字
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		if (nums.empty()) return 0;
+
+		int res = nums[0];
+		for (int i = 1; i < nums.size(); ++i)
+			res = res ^ nums[i];
+
+		return res;
+	}
+};
+
+
+int main()
+{
+
+
+	std::cout << std::endl;
+	return 0;
+}
