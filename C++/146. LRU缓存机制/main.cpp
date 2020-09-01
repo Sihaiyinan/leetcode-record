06fc0e2ea5c203b3ec063b45229ca279a1673123
Sihaiyinan <xu.wh@outlook.com>
2020-09-01 15:32:07 +0800

    146. LRU缓存机制

--- C++/146. LRU缓存机制/main.cpp
+++ C++/146. LRU缓存机制/main.cpp
@@ -1,76 +1,161 @@
-﻿// 146. LRU缓存机制
+// LRU (Least Recently Used)缓存机制
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <numeric>
+#include <ctime>
 #include <unordered_map>
-#include <list>
+
 
 using namespace std;
 
+// 方法一
+class LRUCache1 {
+public:
+    long long int l = 0;   // 最新次数，每次调用该数值加一，容器中数值最低的表示最少使用
+    int cap;   // 容器大小
+    map<int, pair<int, int>> m;   // map<key, pair<val, level>>
 
-struct Value
-{
-	int value;
-	list<int>::iterator iter;
+    LRUCache1(int capacity) {
+        cap = capacity;
+    }
 
-	Value(int v, list<int>::iterator it) : value(v), iter(it) {}
+    int get(int key) {
+        if (m.find(key) == m.end()) return -1;   // 如果不存在返回-1，否则更新key对应的level
+        m[key].second = ++l;
+        return m[key].first;
+    }
+
+    void put(int key, int value) {
+        if (m.find(key) != m.end())   // 如果key已经存在，更新key对应的val和level
+        {
+            m[key] = make_pair(value, ++l);
+            return;
+        }
+        if (m.size() < cap)   // 如果小于容量值，添加key对应的值，否则找出level最小的删除
+        {
+            m[key] = make_pair(value, ++l);
+        } else
+        {
+            auto t = m.begin();
+            for (auto it = m.begin(); it != m.end(); ++it)
+            {
+                if (it->second.second < t->second.second)
+                {
+                    t = it;
+                }
+            }
+            m.erase(t);
+            m[key] = make_pair(value, ++l);
+        }
+    }
 };
+
+
+struct DeListNode
+{
+    int key, val;
+    DeListNode *prev, *next;
+    DeListNode() : key(0), val(0), prev(nullptr), next(nullptr) {}
+    DeListNode(int k, int v) : key(k), val(v), prev(nullptr), next(nullptr) {}
+};
+
 
 class LRUCache {
 public:
-	LRUCache(int capacity) {
-		m_capacity = capacity;
-		m_hash.reserve(capacity * 2); //减少rehash
-	}
+    unordered_map<int, DeListNode*> m;
+    DeListNode *head, *tail;
+    int size;
+    int cap;
 
-	int get(int key) {
-		std::unordered_map<int, Value>::iterator iter = m_hash.find(key);
-		if (iter != m_hash.end()) //找到了
-		{
-			//将该链表节点移动到链表头节点
-			m_list.splice(m_list.begin(), m_list, iter->second.iter);
+    LRUCache(int capacity) {
+        cap = capacity;
+        size = 0;
+        head = new DeListNode();
+        tail = new DeListNode();
+        head->next = tail;
+        tail->prev = head;
+    }
 
-			return iter->second.value;
-		}
-		else
-		{
-			return -1;
-		}
-	}
+    int get(int key) {
+        if (!m.count(key)) return -1;
+        DeListNode *tmp = m[key];
+        move_to_head(tmp);
+        return tmp->val;
+    }
 
-	void put(int key, int value) {
+    void put(int key, int value) {
+        if (m.count(key))
+        {
+            DeListNode *tmp = m[key];
+            tmp->val = value;
+            move_to_head(tmp);
+            return;
+        }
 
-		std::unordered_map<int, Value>::iterator iter = m_hash.find(key);
-		if (iter != m_hash.end()) //找到了
-		{
-			//将该链表节点移动到链表头节点
-			m_list.splice(m_list.begin(), m_list, iter->second.iter);
+        DeListNode *node = new DeListNode(key, value);
+        m[key] = node;
+        add_to_head(node);
+        ++size;
+        if (size > cap)
+        {
+            DeListNode *re = remove_tail();
+            m.erase(re->key);
+            --size;
+        }
+    }
 
-			iter->second.value = value;
-		}
-		else //没找到
-		{
-			if (m_hash.size() == m_capacity) //缓存满了
-			{
-				//缓存淘汰           
-				m_hash.erase(m_list.back()); //hash删除该尾结点的键值对
-				m_list.pop_back(); //删除尾结点
-			}
-			m_list.push_front(key); //在链表头插入新节点
-			Value v(value, m_list.begin());
-			m_hash.insert(pair<int, Value>(key, v)); //hash插入
-		}
-	}
+    void add_to_head(DeListNode *node)
+    {
+        node->prev = head;
+        node->next = head->next;
+        head->next->prev = node;
+        head->next = node;
+    }
 
+    void remove_node(DeListNode *node)
+    {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
 
-private:
-	unordered_map<int, Value> m_hash;
-	list<int> m_list; //存key
-	int m_capacity;
+    void move_to_head(DeListNode *node)
+    {
+        remove_node(node);
+        add_to_head(node);
+    }
+
+    DeListNode *remove_tail()
+    {
+        DeListNode *tmp = tail->prev;
+        remove_node(tmp);
+        return tmp;
+    }
 };
+
+
 
 int main()
 {
+    LRUCache* obj = new LRUCache(2);
+    obj->put(1,1);
+    obj->put(2,2);
+    cout << obj->get(1) << endl;
+    obj->put(3,3);
+    cout << obj->get(2) << endl;
+    obj->put(4,4);
+    cout << obj->get(1) << endl;
+    cout << obj->get(3) << endl;
+    cout << obj->get(4) << endl;
 
-	return 0;
+
+    return 0;
 }
+
+
+
